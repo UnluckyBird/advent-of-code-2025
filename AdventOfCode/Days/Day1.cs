f9d94a2f4c876562fe8705c2f446069efa9308c3
UnluckyBird <martin.justinussen@gmail.com>
2025-12-01 07:32:14 +0100

    Day1

---
+++ AdventOfCode/Days/Day1.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode.Days
+{
+    public class Day1
+    {
+        public static int Part1()
+        {
+            int result = 0;
+            int pos = 50;
+
+            string[] inputs = File.ReadAllLines(AppContext.BaseDirectory + "\\Data\\Day1\\input.txt");
+
+            foreach (string input in inputs)
+            {
+                int rot = int.Parse(input[1..]);
+                if (input[0] == 'L')
+                {
+                    pos = (pos - rot) % 100;
+                }
+                else if (input[0] == 'R')
+                {
+                    pos = (pos + rot) % 100;
+                }
+
+                if (pos == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        public static int Part2()
+        {
+            int result = 0;
+            int pos = 50;
+
+            string[] inputs = File.ReadAllLines(AppContext.BaseDirectory + "\\Data\\Day1\\input.txt");
+
+            foreach (string input in inputs)
+            {
+                int rot = int.Parse(input[1..]);
+                if (input[0] == 'L')
+                {
+                    pos = (pos - rot);
+                }
+                else if (input[0] == 'R')
+                {
+                    pos = (pos + rot);
+                }
+
+                if (pos <= 0 && pos + rot != 0)
+                {
+                    result++;
+                }
+                result += Math.Abs(pos / 100);
+                pos = pos % 100;
+                if (pos < 0)
+                {
+                    pos += 100;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Days;
+using System.Diagnostics;
+
+long startTime = Stopwatch.GetTimestamp();
+Console.WriteLine("********************************************************");
+Console.WriteLine("** ADVENT OF CODE 2025 BY MARTIN DE FRIES JUSTINUSSEN **");
+Console.WriteLine("********************************************************");
+//Day 1
+Console.WriteLine("Day 1.1: " + Day1.Part1());
+Console.WriteLine("Day 1.2: " + Day1.Part2());
+Console.WriteLine($"Day 1 Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
+Console.WriteLine($"Total Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
+
+Console.ReadKey();
