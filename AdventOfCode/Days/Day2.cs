9181b7009c5c707702e8ff267c8dcc95e7e11b54
UnluckyBird <martin.justinussen@gmail.com>
2025-12-02 07:32:50 +0100

    Day2

---
+++ AdventOfCode/Days/Day2.cs
@@ -0,0 +1,59 @@
+﻿namespace AdventOfCode.Days
+{
+    public class Day2
+    {
+        public static long Part1()
+        {
+            long result = 0;
+
+            string[] inputs = File.ReadAllLines(AppContext.BaseDirectory + "\\Data\\Day2\\input.txt");
+
+            var ranges = inputs[0].Split(',').Select(r => r.Split('-').Select(long.Parse).ToArray()).ToArray();
+
+            foreach (var range in ranges)
+            {
+                for (long i = range[0]; i <= range[1]; i++)
+                {
+                    string id = i.ToString();
+                    if (id.Length %  2 == 0 && id[..(id.Length/2)] == id[(id.Length / 2)..])
+                    {
+                        result += i;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static long Part2()
+        {
+            long result = 0;
+
+            string[] inputs = File.ReadAllLines(AppContext.BaseDirectory + "\\Data\\Day2\\input.txt");
+
+            var ranges = inputs[0].Split(',').Select(r => r.Split('-').Select(long.Parse).ToArray()).ToArray();
+
+            foreach (var range in ranges)
+            {
+                for (long i = range[0]; i <= range[1]; i++)
+                {
+                    string id = i.ToString();
+                    for (int j = 1; j < id.Length; j++)
+                    {
+                        if (id.Length % j == 0)
+                        {
+                            var chunks = Enumerable.Range(0, id.Length / j).Select(i => id.Substring(i * j, j)).ToHashSet();
+                            if (chunks.Count == 1)
+                            {
+                                result += i;
+                                break;
+                            }        
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,4 +11,10 @@ Console.WriteLine("Day 1.2: " + Day1.Part2());
 Console.WriteLine($"Day 1 Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
 Console.WriteLine($"Total Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
 
+//Day 2
+Console.WriteLine("Day 2.1: " + Day2.Part1());
+Console.WriteLine("Day 2.2: " + Day2.Part2());
+Console.WriteLine($"Day 2 Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
+Console.WriteLine($"Total Execution Time: {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
+
 Console.ReadKey();
